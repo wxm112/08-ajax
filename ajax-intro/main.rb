3c4b08f9c94e90427d5bc2a7f4bef617dba43d2f
王雪梅 <wxm112@me.com>
2015-02-23 16:25:23 +1100

    added ajax

---
+++ ajax-intro/main.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  erb :index
+end
+
+
+get '/bros' do
+  %w{Gummo}.sample
+end
+
+get '/lotto' do
+  Random.rand(1..40).to_s
+end
+
+
+get '/slow' do
+ sleep 10
+ "haha" * Random.rand(1..10)
+end
---
+++ ajax-intro/public/js/ajax.js
@@ -0,0 +1,18 @@
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function () {
+  console.log('readyState has changed');
+  console.log(this.readyState);
+  console.log(this.responseText);
+
+  if (this.readyState === 4) {
+    $('h3').text( this.responseText );
+    console.log('Done');
+  }
+};
+
+
+request.open('GET', '/slow');
+request.send(); //Asynchronous
+
+console.log('AJAX request is probably still running');
